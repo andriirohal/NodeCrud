914461714892fabf1046c18ec110825b339799d9
Andrii Rohal <andrewrohal@gmail.com>
2026-07-03 13:50:42 +0200

    improve routes folder structure

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,8 +1,8 @@
-import { createProductController, getAllProductsController, getProductByIdController, deleteProductController, updateProductController } from "../controllers";
-import { validateId } from "../middlewares";
-
 import { Router } from "express";
 export const router = Router();
+
+import { createProductController, getAllProductsController, getProductByIdController, deleteProductController, updateProductController } from "../controllers";
+import { validateId } from "../middlewares";
 
 router.get("/", getAllProductsController);
 router.get("/:id", validateId, getProductByIdController);
