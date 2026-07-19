90c53718cd7051b7bc38e847a790db80986300f3
Andrii Rohal <andrewrohal@gmail.com>
2026-07-19 14:31:34 +0300

    improve middlewares folder structure

--- src/middlewares/product.middleware.ts
+++ src/middlewares/product.middleware.ts
@@ -2,28 +2,26 @@ import { Request, Response, NextFunction } from "express";
 
 const UUID_REDEX = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
 
-export const validateId = (req: Request<{id: string}>, res: Response, next: NextFunction): void => {
+export const validateId = (req: Request<{id: string}>, res: Response, next: NextFunction) => {
   const id = req.params.id;
   
   if(!UUID_REDEX.test(id)) {
-    res.status(400).json({
+    return res.status(400).json({
       success: false,
       error: "Invalid product ID",
       status: 400
     });
-    return;
   };
 
   next();
 };
 
-export const errorHandler = (error: unknown, _req: Request, res: Response, _next: NextFunction): void => {
+export const errorHandler = (error: unknown, _req: Request, res: Response, _next: NextFunction) => {
   console.error(error);
 
-  res.status(500).json({
+  return res.status(500).json({
     success: false,
     error: "Internal server error",
     status: 500
   });
-  return;
 };
